ff7604689db01d52a9b757dbbaaef483afe21d38
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-05-22 07:26:58 +0000

    created first file

---
+++ week1-C/helloworld.c
@@ -0,0 +1,8 @@
+ #include <stdio.h>
+
+ int main(void) {
+    printf("hello world \n");
+    // You can use escape characters to print out special characters
+    // \n, \\, \', \"
+    printf("hello \"world\" \n");
+ }
