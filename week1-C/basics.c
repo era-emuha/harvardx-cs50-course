5ec7586e7d482e1c606d76b1931f58c201c1388a
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-05-22 20:28:55 +0000

    updated basics

--- week1-C/basics.c
+++ week1-C/basics.c
@@ -1,29 +1,65 @@
- // Including libraries
- #include <stdio.h>
+// Including libraries
+#include <stdio.h>
 
- int main(void) {
-    printf("hello world \n");
-    // You can use escape characters to print out special characters
-    // \n, \\, \', \"
-    printf("hello \"world\" \n");
+// function definition
+void helloworld(void)
+{
+  printf("Hello World\n");
+}
 
-    // if conditionals
-    int x = 2;
-    int y = 4;
-    
-    if (x > y)
-    {
-      /* code */
-      printf("x is greater than y");
-    } else if (x < y) {
-      printf("x is less than y");
-    } else if (x == y) {
-      printf("x is equal to y");
-    } else {
-      printf("other thing")
-    }
-    
- }
+// function definition with parameter
+void intFunc(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    /* code */
+    printf("Hello\n");
+  }
+}
 
- 
- 
+int main(void)
+{
+  // You can use escape characters to print out special characters
+  // \n, \\, \', \"
+  printf("hello world \n");
+  printf("hello \"world\" \n");
+
+  // conditional operator
+  // if conditional
+  int x = 2;
+  int y = 4;
+
+  if (x > y)
+  {
+    /* code */
+    printf("x is greater than y\n");
+  }
+  else if (x < y)
+  {
+    printf("x is less than y\n");
+  }
+  else if (x == y)
+  {
+    printf("x is equal to y\n");
+  }
+  else
+  {
+    printf("other thing\n");
+  }
+
+  // assignment operator
+  int count = 0;
+  count = count + 1;
+  count += 1;
+  count++;
+
+  // iteration operator
+  // for loop
+  for (int i = 0; i < 10; i++)
+  {
+    /* code */
+    helloworld();
+  }
+
+  intFunc(5);
+}
