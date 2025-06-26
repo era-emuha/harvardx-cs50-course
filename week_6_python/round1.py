e2133c2b0ba967898aac578eb579f16e29761e46
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-06-26 08:49:09 +0000

    Add round1.py with spaced output of characters from a phrase

---
+++ week_6_python/round1.py
@@ -0,0 +1,9 @@
+my_phrase = "You're a wizard, Era!"
+
+print(f"Round 1: ", end="")
+
+for i in range(0, len(my_phrase), 2):
+    #print(my_phrase[i], end="")
+    # Spaced out
+    print(my_phrase[i], end=" ")
+print("")
