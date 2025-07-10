a48b3bda849c602d2b47325ff28468be257468ac
Era Muha <142845027+era-emuha@users.noreply.github.com>
2025-07-10 16:56:40 +0000

    Implement greeting feature with dynamic name input and create greet.html template

--- week_9_flask/app.py
+++ week_9_flask/app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    my_name = request.args.get("my_name", "")
+    my_name = request.args.get("name", "Era")
     return render_template("index.html", name=my_name)
+
+@app.route("/greet")
+def greet():
+    my_name = request.args.get("name", "")
+    return render_template("greet.html", name=my_name)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
